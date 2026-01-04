e1a2f488e996f9f7f4d9a36cdfcb769be87a5be2
Darek <jonskid44@gmail.com>
2026-01-04 14:18:03 +0100

    Added basic warning system, to be improved later

--- report_builder.py
+++ report_builder.py
@@ -24,6 +24,27 @@ def analyze_csv(filepath: str, delimiter: str):
     total_cells = rows * cols
     overall_missing_pct = (total_missing_cells / total_cells) * 100 if total_cells else 0.0
 
+    columns_with_missing = (missing_df["missing_count"] > 0).sum()  
+
+    # Initializaion of warning list
+    # It'll potentially contain bullet-points warnings about data 
+    warnings = []
+
+    duplicated_rows = df.duplicated(keep='first').sum()
+    duplicated_rows_pct = (duplicated_rows / rows) * 100 
+
+    if duplicated_rows > 0:
+        warnings.append(f"Duplicated rows detected: {duplicated_rows} ({duplicated_rows_pct}%) ❗")
+    else:
+        warnings.append("No duplicate rows detected ✅")
+    
+    if columns_with_missing > 0:
+        warnings.append(f"{columns_with_missing} columns have missing values.")
+        worst_column = missing_df.sort_values("missing_count", ascending=False).index[0]
+        warnings.append(f"Worst column(highest missing %) is: {worst_column} with {missing_df.sort_values("missing_count", ascending=False)["missing_count"][0]}% ❗")
+    else: 
+        warnings.append("No missing values detected in any columns ✅")
+
     return (
         rows,
         cols,
